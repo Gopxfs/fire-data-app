f91077cb8200200db03c17bbb1ae3e4faefa2724
Gabriel <gopxfs@hotmail.com>
2022-07-25 17:09:14 -0300

    Restructured countries list file

---
+++ src/components/CountriesList.js
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import getCountryFireCountThunk from '../redux/Thunks';
+
+let load = true;
+
+const CountriesList = () => {
+  const dispatch = useDispatch();
+  if (load)  {
+    load = false;
+    dispatch(getCountryFireCountThunk());
+  }
+
+  const countryCount = useSelector((state) => state.country.countryCount);
+  let countryList = [];
+  let counter = 0;
+  countryCount.forEach((country) => {
+    countryList.push(<li><Link to="country">{country[0]}: {country[1]}</Link></li>);
+    counter += country[1];
+  })
+
+  return (
+    <>
+      <h2>Daily focused fire count in South America: {counter}</h2>
+      <p>STATS BY COUNTRY</p>
+      <ul>
+        {countryList}
+      </ul>
+    </>
+  );
+};
+
+export default CountriesList;
