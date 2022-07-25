7547172de13b58b9c0cddd76bb52060c8230f50c
Gabriel <gopxfs@hotmail.com>
2022-07-25 14:34:05 -0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/HomePage";
-import CountryPage from "./pages/CountryPage";
-import StatePage from "./pages/StatePage";
-import CityPage from "./pages/CityPage";
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import CountryPage from './pages/CountryPage';
+import StatePage from './pages/StatePage';
+import CityPage from './pages/CityPage';
+import AboutPage from './pages/AboutPage';
 
 class App extends React.PureComponent {
   render() {
     return (
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/country" element={<CountryPage />} />
           <Route path="/state" element={<StatePage />} />
           <Route path="/city" element={<CityPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Router>
     );
