07eeee0f8e5b65c6990ec6da53e66e6a3676bcac
Gabriel <gopxfs@hotmail.com>
2022-07-25 15:15:46 -0300

    Add thunk to get data and update state

--- src/redux/CountrySlice.js
+++ src/redux/CountrySlice.js
@@ -1,12 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getCountryFireCountThunk = createAsyncThunk(
+  'fireData/getData',
+  async () => {
+    const response = await fetch('https://queimadas.dgi.inpe.br/api/focos/count')
+      .then((response) => response.json());
+    return response;
+  },
+);
 
 const CountrySlice = createSlice({
   name: 'country',
   initialState: {
-
+    countryCount: [],
   },
   reducers: {
 
+  },
+  extraReducers: {
+    [getCountryFireCountThunk.fulfilled]: (state, { payload }) => {
+      return {
+        countryCount: Object.entries(payload),
+      };
+    },
   },
 });
 
