d6a2b6e4f151447fa4b4e1ff087a4ba1ddc872f2
Gabriel <gopxfs@hotmail.com>
2022-07-25 15:16:15 -0300

    Add display to focused fire counter in south america

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,20 @@
-const HomePage = () => (
-  <p>Home Page</p>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCountryFireCountThunk } from '../redux/CountrySlice';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCountryFireCountThunk());
+  }, []);
+  const countryCount = useSelector((state) => state.country.countryCount);
+  console.log(countryCount);
+  let counter = 0;
+  countryCount.forEach((country) => {
+    counter += country[1];
+  })
+
+  return (<p>Daily focused fire count in South America: {counter}</p>);
+};
 
 export default HomePage;
