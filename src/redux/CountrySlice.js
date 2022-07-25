f6922ec5e6e0477ce74dce7ba5a2dd78094945a8
Gabriel <gopxfs@hotmail.com>
2022-07-25 17:10:21 -0300

    Add state list and removed thunk

--- src/redux/CountrySlice.js
+++ src/redux/CountrySlice.js
@@ -1,21 +1,11 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-export const getCountryFireCountThunk = createAsyncThunk(
-  'fireData/getData',
-  async () => {
-    const response = await fetch('https://queimadas.dgi.inpe.br/api/focos/count')
-      .then((response) => response.json());
-    return response;
-  },
-);
+import { createSlice } from '@reduxjs/toolkit';
+import { getCountryFireCountThunk } from './Thunks';
 
 const CountrySlice = createSlice({
   name: 'country',
   initialState: {
     countryCount: [],
-  },
-  reducers: {
-
+    countryStates: [],
   },
   extraReducers: {
     [getCountryFireCountThunk.fulfilled]: (state, { payload }) => {
