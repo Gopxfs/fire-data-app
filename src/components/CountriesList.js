c0fbcc755acf2c4e549eb8f109d787be4d5799d5
Gabriel <gopxfs@hotmail.com>
2022-07-25 17:51:11 -0300

    Add function to get the ID of the country and dispatch it to get the list of states

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,8 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getCountryFireCountThunk, getCountriesThunk } from '../redux/Thunks';
+import { getCountryFireCountThunk, getCountriesThunk, getStatesThunk } from '../redux/Thunks';
 
 let load = true;
+
+const getCountryID = (country, list) => {
+  for (let i = 0; i < list.length; i += 1) {
+    if(list[i].pais_name === country)
+    return list[i].pais_id;
+  }
+  return 'Not found';
+};
 
 const CountriesList = () => {
   const dispatch = useDispatch();
@@ -12,12 +20,13 @@ const CountriesList = () => {
     dispatch(getCountriesThunk());
   }
 
+  const IDList = useSelector((state) => state.country.countriesList);
   const countryCount = useSelector((state) => state.country.countryCount);
   const countryList = [];
   let counter = 0;
   countryCount.forEach((country) => {
     countryList.push(
-      <li>
+      <li onClick={() => {dispatch(getStatesThunk(getCountryID(country[0], IDList)))}}>
         <Link to="country">
           {country[0]}
           :
