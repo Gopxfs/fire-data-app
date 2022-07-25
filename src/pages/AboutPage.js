86c84bf8ecf6b077e659505fe977ce95aaff8980
Gabriel <gopxfs@hotmail.com>
2022-07-25 14:32:58 -0300

    Add about information

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+const AboutPage = () => (
+  <>
+    <h2>About</h2>
+    <p>
+      This application showcases the number of daily focused fires in South America, allowing
+      the user to specify the data by country, state, and city.
+      All information is gathered from the INPE (National Institute for Space Research),
+      {' '}
+      <a href="https://queimadas.dgi.inpe.br/queimadas/dados-abertos/#">click here to learn more.</a>
+    </p>
+  </>
+);
+
+export default AboutPage;
