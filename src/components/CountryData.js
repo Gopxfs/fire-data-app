e54e690a50320012a8251578ea3df2dc067c7b4d
Gabriel <gopxfs@hotmail.com>
2022-07-26 15:02:35 -0300

    Calculate country percentage

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -3,11 +3,12 @@ import { useSelector } from "react-redux";
 const CountryData = (props) => {
   const totalFires = useSelector((state) => state.country.totalFires);
   const countryFires = useSelector((state) => state.country.currentCountryFires);
+  const country = useSelector((state) => state.country.currentCountry);
+  const percentage = (countryFires/totalFires)*100;
 
   return (
     <>
-      <p>{totalFires}</p>
-      <p>{countryFires}</p>
+      <h2>{country} currently corresponds to {(Math.round(percentage*100)/100).toFixed(2)}% of the total fire cases.</h2>
     </>
   )
 }
