1e30dd04d187c3cd20b2d1c48331d4f41f160431
Gabriel <gopxfs@hotmail.com>
2022-07-27 18:21:50 -0300

    Add hamburger menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleHamburger } from '../redux/FunctionalitySlice';
 
-const Navbar = () => (
-  <>
-    <NavLink to="/">
-      HomePage
-    </NavLink>
-    <NavLink to="/about">
-      About
-    </NavLink>
-  </>
-);
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const isHamburgerOpened = useSelector((state) => state.functionality.isHamburgerOpened);
+
+  return (
+    <nav>
+      {isHamburgerOpened
+        ? (
+          <>
+            <button type="button" className="hamburger" onClick={() => { dispatch(toggleHamburger()); }}> </button>
+            <div id="navLinks" className="hidden">
+              <NavLink to="/">
+                HomePage
+              </NavLink>
+              <NavLink to="/about">
+                About
+              </NavLink>
+            </div>
+          </>
+        )
+        : (
+          <>
+            <button type="button" className="hamburger" onClick={() => { dispatch(toggleHamburger()); }}> </button>
+          </>
+        )}
+    </nav>
+  );
+};
 
 export default Navbar;
