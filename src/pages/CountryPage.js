3c0e4dac3da8234926ed7441d6b75c2498e6ac32
Gabriel <gopxfs@hotmail.com>
2022-07-26 23:11:23 -0300

    Add back button

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
 import StateList from '../components/StateList';
 import CountryData from '../components/CountryData';
 
 const CountryPage = () => (
   <>
+    <Link to="/">Back</Link>
     <CountryData />
     <p>Individual state contribution:</p>
     <StateList />
